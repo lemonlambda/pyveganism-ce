75f5b9692b713909e6c3dff0c4bc0b68953379c3
lemonlambda <lemonjameslem@protonmail.com>
2025-06-30 13:04:35 -0400

    Added hardcore vegetarian back

--- prototypes/updates/hardcore-modes.lua
+++ prototypes/updates/hardcore-modes.lua
@@ -6,20 +6,107 @@ if not vegan_mode and not vegetarian_mode then
 end
 
 local nonvegan_recipes = {
-    --pyht
-    "auog-paddock"
 }
 
 local nonvegetarian_recipes = {
-    --pycp
-    "mukmoux-pasture",
-    "ulric-coral",
-    --pyfe
-    "xyhiphoe-pool",
-    "kmauts-enclosure",
-    --pyht
-    "zipir"
+    "full-render-ulrics",
+    "extract-ulric-eye",
+    "full-render-auogs",
+    "extract-auog-eye",
+    "full-render-mukmoux",
+    "extract-mukmoux-eye",
+    "full-render-vrauks",
+    "full-render-arthurian",
+    "full-render-dhilmoss",
+    "extract-dhilmos-eye",
+    "full-render-scrondrixs",
+    "extract-scrondrix-eye",
+    "full-render-phadais",
+    "full-render-fish",
+    "full-render-dingrits",
+    "full-render-kmauts",
+    "full-render-vonix",
+    "full-render-phagnots",
+    "full-render-xenos",
+    "full-render-antelope",
+    "full-render-zipir",
+    "full-render-trit",
+    "full-render-xyhiphoe",
+    "full-render-simik",
+    "full-render-kor",
+    "full-render-cottongut",
+    "full-render-arqads",
+    "full-render-num",
+    "full-render-zun",
+
+    -- misc stuff
+    "magnetic-organ",
+    "ex-used-auog",
+    "glandular-myocluster",
+    "polynuclear-ganglion",
+    "autoantigens",
+    "intestinal-ee",
+    "subdermal-chemosnare",
+    "snarer-heart",
+    "geostabilization-tissue",
+    "cognition-osteochain",
+    "dimensional-gastricorg",
+    "adaptable-automucosa",
+    "hormonal",
+    "cryogland",
+    "lcc",
+    "aeroorgan",
+    "vsk",
+    "adrenal-cortex",
+    "ex-pelt-din",
+    "ex-used-dingrits",
+    "ex-chi-xeno",
+    "sternite-lung",
+    "ex-chi-arq",
+    "ex-venom-arq",
 }
+
+local animals = {
+    "ulr",
+    "auog",
+    "muk",
+    "vrauks",
+    "art",
+    "dhi",
+    "scro",
+    "pha",
+    "din",
+    "kma",
+    "von",
+    "phag",
+    "xeno",
+    "zipir",
+    "trit",
+    "xyh",
+    "sim",
+    "khr",
+    "cot",
+    "kor",
+    "arq",
+    "num",
+    "zun",
+}
+
+local prefixes = {
+    "ex-blo",
+    "ex-bon",
+    "ex-bra",
+    "ex-fat",
+    "ex-gut",
+    "ex-me",
+    "ex-ski"
+}
+
+for _,animal in pairs(animals) do
+    for _,prefix in pairs(prefixes) do
+        table.insert(nonvegetarian_recipes, prefix .. "-" .. animal)
+    end
+end
 
 local function disable_recipe(name)
     --seems like I don't need to check if the recipe exists
@@ -29,7 +116,7 @@ local function disable_recipe(name)
     --setting them to hidden makes the player unable to craft them
     --disabling them would be harder to do because you would need to find all the technologies that unlock them
     --deleting them would break all mods who try to change the recipe in later stages
-    recipe.hidden = true
+    recipe:set_fields{ hidden = true }
 end
 
 function each(tbl, callback)
