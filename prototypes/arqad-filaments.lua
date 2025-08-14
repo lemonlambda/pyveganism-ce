3a2ca34cb09cdc665585b7e0ca912e0e9448b9d3
lambdalemon <lemonjameslem@protonmail.com>
2025-08-14 05:37:00 -0400

    ngl I just forget to commit for a bit

--- prototypes/arqad-filaments.lua
+++ prototypes/arqad-filaments.lua
@@ -115,10 +115,355 @@ py_veganism_globals.create_recipe(
 )
 
 -- Honey making
+
+FLUID {
+  type = "fluid",
+  name = "flora-solution",
+  icons = {
+    {icon = "__base__/graphics/icons/fluid/water.png"},
+    {icon = "__pyalienlifegraphics__/graphics/icons/mip/bio/03.png", icon_size = 64, scale = 0.25, shift = {8, 8}}
+  },
+  default_temperature = 15,
+  base_color = {156/255, 203/255, 44/255},
+  flow_color = {156/255, 203/255, 44/255},
+}
+
+FLUID {
+  type = "fluid",
+  name = "moondrop-solution",
+  icons = {
+    {icon = "__base__/graphics/icons/fluid/water.png"},
+    {icon = "__pyalienlifegraphics__/graphics/icons/moondrop.png", icon_size = 64, scale = 0.25, shift = {8, 8}}
+  },
+  default_temperature = 15,
+  base_color = {91/255, 158/255, 198/255},
+  flow_color = {91/255, 158/255, 198/255},
+}
+
+FLUID {
+  type = "fluid",
+  name = "ralesia-solution",
+  icons = {
+    {icon = "__base__/graphics/icons/fluid/water.png"},
+    {icon = "__pyalienlifegraphics__/graphics/icons/ralesia.png", icon_size = 64, scale = 0.25, shift = {8, 8}}
+  },
+  default_temperature = 15,
+  base_color = {1, 187/255, 0},
+  flow_color = {1, 187/255, 0},
+}
+
+FLUID {
+  type = "fluid",
+  name = "nectar-1",
+  icons = {
+    {icon = "__pyveganism__/graphics/icons/nectar.png"},
+    {icon = "__pyalienlifegraphics__/graphics/icons/1.png", icon_size = 64, scale = 0.25, shift = {8, 8}}
+  },
+  default_temperature = 15,
+  base_color = {174/255, 129/255, 84/255},
+  flow_color = {174/255, 129/255, 84/255},
+}
+
+FLUID {
+  type = "fluid",
+  name = "nectar-2",
+  icons = {
+    {icon = "__pyveganism__/graphics/icons/nectar.png"},
+    {icon = "__pyalienlifegraphics__/graphics/icons/2.png", icon_size = 64, scale = 0.25, shift = {8, 8}}
+  },
+  default_temperature = 15,
+  base_color = {174/255, 129/255, 84/255},
+  flow_color = {174/255, 129/255, 84/255},
+}
+
+FLUID {
+  type = "fluid",
+  name = "nectar-3",
+  icons = {
+    {icon = "__pyveganism__/graphics/icons/nectar.png"},
+    {icon = "__pyalienlifegraphics__/graphics/icons/3.png", icon_size = 64, scale = 0.25, shift = {8, 8}}
+  },
+  default_temperature = 15,
+  base_color = {174/255, 129/255, 84/255},
+  flow_color = {174/255, 129/255, 84/255},
+}
+
 RECIPE {
   type = "recipe",
-  name = "dichloromethane",
+  name = "chloridemethane-from-cog",
+  subgroup = "py-veganism-arqad",
+  category = "chemistry",
   ingredients = {
-  
-  }
-}
+    {
+      type = "fluid",
+      name = "coke-oven-gas",
+      amount = 100,
+    },
+    {
+      type = "fluid",
+      name = "chlorine",
+      amount = 100
+    }
+  },
+  results = {
+    {
+      type = "fluid",
+      name = "chloromethane",
+      amount = 100
+    },
+    {
+      type = "fluid",
+      name = "hydrogen-chloride",
+      amount = 100
+    },
+  },
+  main_product = "chloromethane"
+}:add_unlock("coke-mk01")
+
+RECIPE {
+  type = "recipe",
+  name = "basic-dichloromethane",
+  subgroup = "py-veganism-arqad",
+  category = "chemistry",
+  ingredients = {
+    {
+      type = "fluid",
+      name = "chloromethane",
+      amount = 100
+    },
+    {
+      type = "fluid",
+      name = "chlorine",
+      amount = 100
+    }
+  },
+  results = {
+    {
+      type = "fluid",
+      name = "dcm",
+      amount = 100
+    },
+    {
+      type = "fluid",
+      name = "hydrogen-chloride",
+      amount = 100
+    },
+  },
+  main_product = "dcm"
+}:add_unlock("arqad")
+
+RECIPE {
+  type = "recipe",
+  name = "flora-solution",
+  subgroup = "py-veganism-arqad",
+  category = "biofactory",
+  ingredients = {
+    {
+      type = "item",
+      name = "native-flora",
+      amount = 10
+    },
+    {
+      type = "fluid",
+      name = "dcm",
+      amount = 10
+    }
+  },
+  results = {
+    {
+      type = "fluid",
+      name = "flora-solution",
+      amount = 10
+    }
+  },
+  main_product = "flora-solution"
+}:add_unlock("arqad")
+
+RECIPE {
+  type = "recipe",
+  name = "moondrop-solution",
+  subgroup = "py-veganism-arqad",
+  category = "moon",
+  ingredients = {
+    {
+      type = "item",
+      name = "moondrop",
+      amount = 10
+    },
+    {
+      type = "fluid",
+      name = "benzene",
+      amount = 10
+    }
+  },
+  results = {
+    {
+      type = "fluid",
+      name = "moondrop-solution",
+      amount = 10
+    }
+  },
+  main_product = "moondrop-solution"
+}:add_unlock("arqad")
+
+RECIPE {
+  type = "recipe",
+  name = "ralesia-solution",
+  subgroup = "py-veganism-arqad",
+  category = "ralesia",
+  ingredients = {
+    {
+      type = "item",
+      name = "ralesia",
+      amount = 10
+    },
+    {
+      type = "fluid",
+      name = "dcm",
+      amount = 10
+    }
+  },
+  results = {
+    {
+      type = "fluid",
+      name = "ralesia-solution",
+      amount = 10
+    }
+  },
+  main_product = "ralesia-solution"
+}:add_unlock("arqad")
+
+RECIPE {
+  type = "recipe",
+  name = "separate-flora-solution",
+  subgroup = "py-veganism-arqad",
+  category = "fluid-separator",
+  energy_required = 5,
+  ingredients = {
+    {
+      type = "fluid",
+      name = "flora-solution",
+      amount = 100
+    }
+  },
+  results = {
+    {
+      type = "fluid",
+      name = "nectar-1",
+      amount = 50
+    }
+  },
+  main_product = "nectar-1"
+}:add_unlock("arqad")
+
+RECIPE {
+  type = "recipe",
+  name = "separate-moondrop-solution",
+  subgroup = "py-veganism-arqad",
+  category = "fluid-separator",
+  energy_required = 5,
+  ingredients = {
+    {
+      type = "fluid",
+      name = "moondrop-solution",
+      amount = 100
+    },
+    {
+      type = "fluid",
+      name = "nectar-1",
+      amount = 100
+    }
+  },
+  results = {
+    {
+      type = "fluid",
+      name = "nectar-2",
+      amount = 50
+    }
+  },
+  main_product = "nectar-2"
+}:add_unlock("arqad")
+
+RECIPE {
+  type = "recipe",
+  name = "separate-ralesia-solution",
+  subgroup = "py-veganism-arqad",
+  category = "fluid-separator",
+  energy_required = 5,
+  ingredients = {
+    {
+      type = "fluid",
+      name = "ralesia-solution",
+      amount = 100
+    },
+    {
+      type = "fluid",
+      name = "nectar-2",
+      amount = 100
+    }
+  },
+  results = {
+    {
+      type = "fluid",
+      name = "nectar-3",
+      amount = 50
+    }
+  },
+  main_product = "nectar-3"
+}:add_unlock("arqad")
+
+RECIPE {
+  type = "recipe",
+  name = "arqad-honey-from-nectar",
+  subgroup = "py-veganism-arqad",
+  category = "bio-printer",
+  ingredients = {
+    {
+      type = "fluid",
+      name = "nectar-3",
+      amount = 100
+    },
+    {
+      type = "fluid",
+      name = "hot-air",
+      amount = 100
+    }
+  },
+  results = {
+    {
+      type = "fluid",
+      name = "arqad-honey",
+      amount = 100
+    },
+  },
+  main_product = "arqad-honey"
+}:add_unlock("arqad")
+
+RECIPE {
+  type = "recipe",
+  name = "arqad-honeycomb-filling",
+  subgroup = "py-veganism-arqad",
+  category = "py-barreling",
+  energy_required = 5,
+  ingredients = {
+    {
+      type = "item",
+      name = "empty-honeycomb",
+      amount = 1
+    },
+    {
+      type = "fluid",
+      name = "arqad-honey",
+      amount = 50
+    }
+  },
+  results = {
+    {
+      type = "item",
+      name = "honeycomb",
+      amount = 1
+    }
+  },
+  main_product = "honeycomb"
+}:add_unlock("arqad")
+
