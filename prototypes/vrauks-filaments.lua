d914428de517c707eab11a515635683c3b2c8c83
lambdalemon <lemonjameslem@protonmail.com>
2025-08-18 07:57:39 -0400

    All mks

--- prototypes/vrauks-filaments.lua
+++ prototypes/vrauks-filaments.lua
@@ -10,19 +10,26 @@ py_veganism_globals.create_recipe(
   true
 )
 
-py_veganism_globals.create_recipe(
-  "vrauks-mk02",
-  {{type = "item", name = "vrauk-molt", amount = 2}},
-  py_veganism_globals.hex2rgb("#3b7958"),
+for i=2,4 do
+  py_veganism_globals.create_recipe(
+    "vrauks-mk0" .. i,
+    {{type = "item", name = "vrauk-molt", amount = 2}},
+    py_veganism_globals.hex2rgb("#3b7958"),
 
-  py_veganism_globals.costs,
-  "vrauks-mk02",
-  py_veganism_globals.sub_recipes,
-  {
-    filament_icon_name = "vrauks",
-    mk_level = 2,
-    codex_name = "vrauks-codex",
-    not_nvm = true
-  },
-  true
-)
+    py_veganism_globals.costs,
+    "vrauks-mk0" .. i,
+    py_veganism_globals.sub_recipes,
+    {
+      filament_icon_name = "vrauks",
+      filament_result_probability = 0.005,
+      additional_results = {
+        {type = "fluid", name = "vrauks-filament", amount_min = 100, amount_max = 5000, probability = 0.05}
+      },
+      main_product = "vrauks-mk0" .. i .. "-filament",
+      mk_level = i,
+      codex_name = "vrauks-codex",
+      not_nvm = true
+    },
+    true
+  )
+end
